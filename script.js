bb94753f2c769012c3cace2b89c573c675ef62d7
Sadrunnisa Khan <144330716+Sadrunnisa-Khan@users.noreply.github.com>
2023-09-07 14:51:46 +0530

    Site V1

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('header').innerHTML = header_code;
+
+
+
+let header_code = '<nav class="navbar navbar-dark bg-primary">'+
+    '<div class="container-fluid">'+
+      '<a class="navbar-brand" href="#">'+
+        '<img src="logo.jpg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">'+
+        'EduNaik'+
+      '</a><div style="color:white; font-size:20px;"><img width="25" height="25" src="https://img.icons8.com/arcade/64/phone-disconnected.png" alt="phone-disconnected"/> +91 80973 46128</div>'+
+    '</div>'+
+'</nav>'
